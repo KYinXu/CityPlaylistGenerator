1b7ceff8b984acc58f86ab73980fa9f086553a82
KYinXu <kyleyinxu@gmail.com>
2024-01-26 21:24:31 -0800

    add spotify call

--- spotify-api-call.js
+++ spotify-api-call.js
@@ -4,20 +4,38 @@ and culture of irvine. Include the nature of irvine
 as a city and community, as well as artists who originated from irvine
 */
 BASE_URL = "https://api.spotify.com"
-function createPlaylistUrl(){
-    const xhr = new XMLHttpRequest();
-    req.open('GET', link, true);
-    req.setRequestHeader();
-    req.setRequestData();
-    req.send();
-    //Create the url: https://developer.spotify.com/documentation/web-api/reference/create-playlist
+function createPlaylist(user_id, name){
+    //Reference: https://developer.spotify.com/documentation/web-api/reference/create-playlist
+    url = BASE_URL + '/users/' + user_id + '/playlists';
+    params = {
+        "name": name,
+        "public": true,
+        "collaborative": false
+    }
+    
+    curlPost(url, params, '');
 }
-function callApi(xhr) {
+function curlPost(url, params, token) {
+    /*
+    curl --request POST \
+  --url https://api.spotify.com/v1/users/smedjan/playlists \
+  --header 'Authorization: Bearer 1POdFZRZbvb...qqillRxMr2z' \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "name": "New Playlist",
+    "description": "New playlist description",
+    "public": false
+}'
+    */
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.send(JSON.stringify(params));
     
-    
-    
-    req.onload = ()=> {
-        if (req.status === 200) {
+    xhr.onload = ()=> {
+        if (xhr.status === 200) {
             const response = JSON.parse(req.response)
             console.log(response)
         }
