0a68057f9e5babf8a7c9800829e6613db4290652
cyrusbuffington <cyrusbuffington@gmail.com>
2024-01-27 12:23:33 -0800

    added sqlite db

---
+++ mysql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE track(
+                   trackURI STRING NOT NULL PRIMARY KEY, 
+                   trackName STRING NOT NULL, 
+                   trackArtist STRING NOT NULL);
+
+INSERT INTO track(trackURI, trackName, trackArtist) VALUES ("84aF8494AKDK", "Song Name", "Song Artist");
