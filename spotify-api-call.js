e47f7df58ff4d3e4e07308054eca35481cfd093f
KYinXu <kyleyinxu@gmail.com>
2024-01-26 20:50:26 -0800

    add example request send

--- spotify-api-call.js
+++ spotify-api-call.js
@@ -1,2 +1,20 @@
 //hi
-//OYOOYOYOYO
+BASE_URL = "https://developer.spotify.com/documentation/web-api/reference/"
+function createPlaylistUrl(){
+    //Create the url: https://developer.spotify.com/documentation/web-api/reference/create-playlist
+}
+function callApi(link) {
+    const req = new XMLHttpRequest();
+    req.open('GET', link, true);
+    req.send();
+    
+    req.onload = ()=> {
+        if (req.status === 200) {
+            const response = JSON.parse(req.response)
+            console.log(response)
+        }
+        else {
+            //error
+        }
+    }
+}
