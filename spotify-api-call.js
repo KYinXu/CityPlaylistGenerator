2bc42a31f0bff3bfc4421324ab7b65c0bcb24610
cyrusbuffington <cyrusbuffington@gmail.com>
2024-01-26 20:17:06 -0800

    spotify prompt

--- spotify-api-call.js
+++ spotify-api-call.js
@@ -1,2 +1,9 @@
 //hi
 //OYOOYOYOYO
+
+/*create a spotify playlist that captures the individuality 
+and culture of irvine. Include the nature of irvine 
+as a city and community, as well as artists who originated from irvine
+*/
+
+"
