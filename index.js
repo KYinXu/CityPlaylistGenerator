fd6fcaf6a64bc23afa6ad5f8b695f040d05a3558
KYinXu <kyleyinxu@gmail.com>
2024-01-27 14:45:14 -0800

    add hint label

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function testFunction(){
     console.log("test")
 }
+function showHint(hint_id){
+    var tb = document.getElementById(hint_id);
+    if (tb.style.visibility == "visible") {
+        tb.style.visibility = "hidden";
+    }
+    else {
+        tb.style.visibility = "visible";
+    }
+        
+    console.log('success');
+}
