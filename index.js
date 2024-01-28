54163b3b1b337b406681ef70419af9ffc2b3c930
cyrusbuffington <cyrusbuffington@gmail.com>
2024-01-28 11:01:00 -0800

    gdd

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ const BASE_URL = "https://api.spotify.com/v1";
     }
     request.post(clientServerOptions, function (error, response, body) {
       console.log(response.statusCode);
-      playlist_id = body["id"];
+      playlist_id = body.id;
       console.log(body["id"]);
       console.log(Object.prototype.toString.call(body));
       console.log(body.collaborative);
