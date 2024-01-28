e6c5be6379487a65323abe3e997914a1b4dd70d5
JoshuaJJ11 <joshuajwj11@gmail.com>
2024-01-27 20:22:15 -0800

    new

--- spotify-api-call.js
+++ spotify-api-call.js
@@ -159,81 +159,126 @@ function curlPost(url, params, token) {
     // }
 // })();
 
+var client_id = 'c0ecea08e95a467ab50824a0c9e2e150';
+var redirect_uri = 'http://localhost:8888/callback';
 
+var app = express();
 
-async function _getToken() {
-    console.log("RUN GET TOKEN");
-    const generateRandomString = (length) => {
-        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const values = crypto.getRandomValues(new Uint8Array(length));
-        return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+  
+    if (state === null) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+      var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+      };
     }
-    const codeVerifier  = generateRandomString(64);
+  });
 
-    const sha256 = async (plain) => {
-        const encoder = new TextEncoder()
-        const data = encoder.encode(plain)
-        return window.crypto.subtle.digest('SHA-256', data)
-    }
-    const base64encode = (input) => {
-        return btoa(String.fromCharCode(...new Uint8Array(input)))
-        .replace(/=/g, '')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_');
-    }
-    const hashed = await sha256(codeVerifier)
-    const codeChallenge = base64encode(hashed);
+// async function _getToken() {
+//     console.log("RUN GET TOKEN");
+//     const generateRandomString = (length) => {
+//         const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//         const values = crypto.getRandomValues(new Uint8Array(length));
+//         return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+//     }
+//     const codeVerifier  = generateRandomString(64);
 
-    const clientId = 'c0ecea08e95a467ab50824a0c9e2e150';
-    const redirectUri = 'http://localhost:5500';
+//     const sha256 = async (plain) => {
+//         const encoder = new TextEncoder()
+//         const data = encoder.encode(plain)
+//         return window.crypto.subtle.digest('SHA-256', data)
+//     }
+//     const base64encode = (input) => {
+//         return btoa(String.fromCharCode(...new Uint8Array(input)))
+//         .replace(/=/g, '')
+//         .replace(/\+/g, '-')
+//         .replace(/\//g, '_');
+//     }
+//     const hashed = await sha256(codeVerifier)
+//     const codeChallenge = base64encode(hashed);
 
-    const scope = 'user-read-private user-read-email';
-    const authUrl = new URL("https://accounts.spotify.com/authorize")
+//     const clientId = 'c0ecea08e95a467ab50824a0c9e2e150';
+//     const redirectUri = 'http://localhost:5500';
 
-        // generated in the previous step
-    window.localStorage.setItem('code_verifier', codeVerifier);
+//     const scope = 'user-read-private user-read-email';
+//     const authUrl = new URL("https://accounts.spotify.com/authorize")
 
-    const params =  {
-        response_type: 'code',
-        client_id: clientId,
-        scope,
-        code_challenge_method: 'S256',
-        code_challenge: codeChallenge,
-        redirect_uri: redirectUri,
-    }
+//         // generated in the previous step
+//     window.localStorage.setItem('code_verifier', codeVerifier);
 
-    authUrl.search = new URLSearchParams(params).toString();
-    window.location.href = authUrl.toString();
+//     const params =  {
+//         response_type: 'code',
+//         client_id: clientId,
+//         scope,
+//         code_challenge_method: 'S256',
+//         code_challenge: codeChallenge,
+//         redirect_uri: redirectUri,
+//     }
 
-    const urlParams = new URLSearchParams(window.location.search);
-    let code = urlParams.get('code');
+//     authUrl.search = new URLSearchParams(params).toString();
+//     window.location.href = authUrl.toString();
 
-    const getToken = async code => {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     let code = urlParams.get('code');
 
-        // stored in the previous step
-        let codeVerifier = localStorage.getItem('code_verifier');
+//     const getToken = async code => {
+
+//         // stored in the previous step
+//         let codeVerifier = localStorage.getItem('code_verifier');
         
-        const payload = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({
-                client_id: clientId,
-                grant_type: 'authorization_code',
-                code,
-                redirect_uri: redirectUri,
-                code_verifier: codeVerifier,
-            }),
-        }
+//         const payload = {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//             body: new URLSearchParams({
+//                 client_id: clientId,
+//                 grant_type: 'authorization_code',
+//                 code,
+//                 redirect_uri: redirectUri,
+//                 code_verifier: codeVerifier,
+//             }),
+//         }
         
-        const body = await fetch(url, payload);
-        const response = await body.json();
+//         const body = await fetch(url, payload);
+//         const response = await body.json();
         
-        localStorage.setItem('access_token', response.access_token);
-        const token = localStorage.getItem('access_token');
-    }
-}
+//         localStorage.setItem('access_token', response.access_token);
+//         const token = localStorage.getItem('access_token');
+//     }
+// }
   
 // const client_secret = 'e7c5c2c3246c441b97fc244f2985fdbc';
 
