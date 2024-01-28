6cbbb13fc28ac87fc72f9387822f014561ba21af
cyrusbuffington <cyrusbuffington@gmail.com>
2024-01-27 16:35:55 -0800

    iosgsifos

--- spotify-api-call.js
+++ spotify-api-call.js
@@ -40,7 +40,7 @@ function curlPost(url, params, token) {
     "public": false
 }'
     */
-    const xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Authorization', 'Bearer ' + token);
     xhr.setRequestHeader('Content-Type', 'application/json');
